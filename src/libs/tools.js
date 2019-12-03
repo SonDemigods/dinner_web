aa87a1edad1d10c69dc03bbb62eadeb42f84ae0c
demigods <a13752365641@126.com>
2019-12-03 17:03:06 +0800

    添加工具函数

--- src/libs/tools.js
+++ src/libs/tools.js
@@ -1,3 +1,15 @@
+/**
+ * @description 工具方法
+ * @author 张航
+ * @date 2019-02-01 08:44:31
+ * @version V1.0.0
+ */
+
+/**
+ * @param {Array} arr 遍历的数组
+ * @param {Function} fn 执行的方法
+ * @description 循环数组并执行方法
+ */
 export const forEach = (arr, fn) => {
   if (!arr.length || !fn) return
   let i = -1
@@ -46,7 +58,7 @@ export const hasOneOf = (targetarr, arr) => {
  * @param {String|Number} value 要验证的字符串或数值
  * @param {*} validList 用来验证的列表
  */
-export function oneOf (value, validList) {
+export const oneOf = (value, validList) => {
   for (let i = 0; i < validList.length; i++) {
     if (value === validList[i]) {
       return true
@@ -210,6 +222,312 @@ export const objEqual = (obj1, obj2) => {
   const keysArr2 = Object.keys(obj2)
   if (keysArr1.length !== keysArr2.length) return false
   else if (keysArr1.length === 0 && keysArr2.length === 0) return true
+  else {
+    let flag = true
+    keysArr1.forEach(key => {
+      if (typeof obj1[key] === 'undefined' || typeof obj2[key] === 'undefined') {
+        if (typeof obj1[key] !== 'undefined' || typeof obj2[key] !== 'undefined') {
+          flag = false
+          return false
+        }
+      } else if (Array.isArray(obj1[key]) || Array.isArray(obj2[key])) {
+        if (obj1[key].toString() !== obj2[key].toString()) {
+          flag = false
+          return false
+        }
+      } else {
+        if (obj1[key] !== obj2[key]) {
+          flag = false
+          return false
+        }
+      }
+    })
+    return flag
+  }
+
   /* eslint-disable-next-line */
-  else return !keysArr1.some(key => obj1[key] != obj2[key])
+  /*  else return !keysArr1.some(key => {
+      if (typeof obj1[key] === 'undefined' && typeof obj2[key] === 'undefined') {
+
+      }
+      return obj1[key] != obj2[key]
+    })*/
+}
+
+/**
+ * @param {Date} date 时间对象
+ * @returns {String} 时间字符串
+ */
+export const date2str = date => {
+  let str
+  if (Object.prototype.toString.call(date) === '[object Date]') {
+    let Y = date.getFullYear()
+    let M = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+    let D = date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()
+    str = `${Y}-${M}-${D}`
+  } else {
+    console.warn(`参数"${date}"类型不正确,请传入[Date]类型！`)
+    str = ''
+  }
+  return str
+}
+
+/**
+ * @param {String|Number} str 时间字符串或时间戳
+ * @returns {Date} 转换成Date数据
+ */
+export const str2date = str => {
+  let date
+  if (Object.prototype.toString.call(str) === '[object String]' || Object.prototype.toString.call(str) === '[object Number]') {
+    date = new Date(str)
+  } else {
+    console.warn(`参数"${str}"类型不正确,请传入[String]或[Number]类型！`)
+    date = null
+  }
+  return date
+}
+
+/**
+ * @functionName setLocalStorage
+ * @param {String} name 本地存储key
+ * @param {String} data 本地存储value
+ * @description 存储本地数据
+ * @author 张航
+ * @date 2019-02-03 09:18:09
+ * @version V1.0.0
+ */
+export const setLocalStorage = (name, data) => {
+  localStorage.setItem(name, data)
+}
+
+/**
+ * @functionName getLocalStorage
+ * @param {String} name 本地存储key
+ * @description 获取本地数据
+ * @author 张航
+ * @date 2019-02-03 09:21:32
+ * @version V1.0.0
+ */
+export const getLocalStorage = name => {
+  return localStorage.getItem(name) || ''
+}
+
+/**
+ * @functionName isStatic
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检测数据是不是除了symbol外的原始数据
+ * @author 张航
+ * @date 2019-07-10 09:21:32
+ * @version V1.0.0
+ */
+export const isStatic = (value) => {
+  return (
+    typeof value === 'string' ||
+    typeof value === 'number' ||
+    typeof value === 'boolean' ||
+    typeof value === 'undefined' ||
+    value === null
+  )
+}
+
+/**
+ * @functionName isPrimitive
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检测数据是不是原始数据
+ * @author 张航
+ * @date 2019-07-10 13:42:00
+ * @version V1.0.0
+ */
+export const isPrimitive = (value) => {
+  return isStatic(value) || typeof value === 'symbol'
+}
+
+/**
+ * @functionName isObject
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 判断数据是不是引用类型的数据
+ * @author 张航
+ * @date 2019-07-10 13:43:48
+ * @version V1.0.0
+ */
+export const isObject = (value) => {
+  let type = typeof value
+  return value != null && (type === 'object' || type === 'function')
+}
+
+/**
+ * @functionName isObjectLike
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检查 value 是否是类对象
+ * @author 张航
+ * @date 2019-07-10 13:46:44
+ * @version V1.0.0
+ */
+export const isObjectLike = (value) => {
+  return value != null && typeof value === 'object'
+}
+
+/**
+ * @functionName getRawType
+ * @param {*} value 被检测的数据
+ * @return {String} 数据类型
+ * @description 获取数据类型
+ * @author 张航
+ * @date 2019-07-10 13:48:55
+ * @version V1.0.0
+ */
+export const getRawType = (value) => {
+  return Object.prototype.toString.call(value).slice(8, -1)
+}
+
+/**
+ * @functionName isPlainObject
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 判断数据是不是Object类型的数据
+ * @author 张航
+ * @date 2019-07-10 14:41:44
+ * @version V1.0.0
+ */
+export const isPlainObject = (obj) => {
+  return Object.prototype.toString.call(obj) === '[object Object]'
+}
+
+/**
+ * @functionName isFunction
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检查 value 是不是函数
+ * @author 张航
+ * @date 2019-07-10 14:40:42
+ * @version V1.0.0
+ */
+export const isFunction = (value) => {
+  return Object.prototype.toString.call(value) === '[object Function]'
+}
+
+/**
+ * @functionName isLength
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检查 value 是否为有效的类数组长度
+ * @author 张航
+ * @date 2019-07-10 14:38:53
+ * @version V1.0.0
+ */
+export const isLength = (value) => {
+  return typeof value === 'number' && value > -1 && value % 1 === 0 && value <= Number.MAX_SAFE_INTEGER
+}
+
+/**
+ * @functionName isArrayLike
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检查 value 是否是类数组
+ * @author 张航
+ * @date 2019-07-10 14:36:32
+ * @version V1.0.0
+ */
+export const isArrayLike = (value) => {
+  return value != null && isLength(value.length) && !isFunction(value)
+}
+
+/**
+ * @functionName isEmpty
+ * @param {*} value 被检测的数据
+ * @return {Boolean} 返回结果
+ * @description 检查 value 是否为空
+ * @author 张航
+ * @date 2019-07-10 13:52:21
+ * @version V1.0.0
+ */
+export const isEmpty = (value) => {
+  if (value == null) {
+    return true
+  }
+  if (isArrayLike(value)) {
+    return !value.length
+  } else if (isPlainObject(value)) {
+    for (let key in value) {
+      if (hasOwnProperty.call(value, key)) {
+        return false
+      }
+    }
+    return true
+  }
+  return false
+}
+
+/**
+ * @functionName camelize
+ * @param {String} str 待转换的字符串
+ * @return {String} 转换后的字符串
+ * @description 横线转驼峰命名
+ * @author 张航
+ * @date 2019-07-10 14:09:21
+ * @version V1.0.0
+ */
+export const camelize = (str) => {
+  const camelizeRE = /-(\w)/g
+  return str.replace(camelizeRE, function (_, c) {
+    return c ? c.toUpperCase() : ''
+  })
+}
+
+/**
+ * @functionName hyphenate
+ * @param {String} str 待转换的字符串
+ * @return {String} 转换后的字符串
+ * @description 驼峰命名转横线命名
+ * @author 张航
+ * @date 2019-07-10 14:11:38
+ * @version V1.0.0
+ */
+
+export const hyphenate = (str) => {
+  const hyphenateRE = /\B([A-Z])/g
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+}
+
+/**
+ * @functionName capitalize
+ * @param {String} str 待转换的字符串
+ * @return {String} 转换后的字符串
+ * @description 字符串首位大写
+ * @author 张航
+ * @date 2019-07-10 14:13:54
+ * @version V1.0.0
+ */
+export const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+/**
+ * @functionName isJSON
+ * @param {String} str 待检测的字符串
+ * @return {String} 检测后的结果
+ * @description 是否json字符串
+ * @author 张航
+ * @date 2019-07-10 14:13:54
+ * @version V1.0.0
+ */
+export const isJSON = (str) => {
+  if (typeof str === 'string') {
+    try {
+      var obj = JSON.parse(str)
+      if (typeof obj === 'object' && obj) {
+        return true
+      } else {
+        return false
+      }
+    } catch (e) {
+      console.log('error：' + str + '!!!' + e)
+      return false
+    }
+  }
+  console.error('It is not a string!')
 }
